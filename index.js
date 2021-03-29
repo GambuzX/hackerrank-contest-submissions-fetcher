60dc9b98c53fa8e257d8012b134a8aca5bfcce99
GambuzX <mg_mesquita@hotmail.com>
2021-03-29 12:14:54 +0100

    Saving contest results

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const axios = require('axios');
 const urls = require('./urls');
 require('dotenv').config()
@@ -6,17 +8,41 @@ require('dotenv').config()
 axios.defaults.headers.common['Cookie'] = process.env.COOKIE;
 axios.defaults.headers.common['User-Agent'] = 'scraper'; // this needs to have some value or it returns 403
 
+// global variables
+const contestSlug = process.env.CONTEST;
+const resultsPath = 'results';
+const contestPath = path.join(resultsPath, contestSlug);
 
-async function getContestSubmissions(contestSlug) {
+
+async function getContestSubmissions() {
     const response = await axios.get(urls.contestSubmissions(contestSlug));
     return response.data.models;
+}
+
+function createContestFolder() {
+    if (!fs.existsSync(resultsPath)) {
+        fs.mkdirSync(resultsPath);
+    }
+    if (!fs.existsSync(contestPath)) {
+        fs.mkdirSync(contestPath);
+    }
+}
+
+function saveUsersSubmissions(usersSubmissions) {
+    createContestFolder();
+
+    // save general submissions info
+    const filePath = path.join(contestPath, 'submissions.json');
+    fs.writeFile(filePath, JSON.stringify(usersSubmissions,null,2), (err) => {
+        if (err) console.log(err);
+    });
 }
 
 
 (async () => {
     try {
         // get all contest submissions
-        const submissions = await getContestSubmissions(process.env.CONTEST);
+        const submissions = await getContestSubmissions();
 
         // separate submissions per user
         const submissionsPerUser = submissions.reduce((acc, curr) => {
@@ -28,7 +54,8 @@ async function getContestSubmissions(contestSlug) {
             return acc;
         }, {});
 
-        console.log(submissionsPerUser);
+        // save the submissions of each user in separate folders
+        saveUsersSubmissions(submissionsPerUser);
     }
     catch (error) {
         console.error("An error occured: %d", error.statusCode);
@@ -37,8 +64,7 @@ async function getContestSubmissions(contestSlug) {
 })();
 
 /* TODO
- - create results folder for each user
- - save 'submissions.json' for each user with general info about all user submissions
+ - separate user info per challenge
  - call submissionInfo API and save the code of each user's submission
  - detect users with more than 1 submissions per week
  - detect users copying from each other
