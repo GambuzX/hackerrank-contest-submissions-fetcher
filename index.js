cc71b9c2b5c3230663df6691a1c990467bc801bf
GambuzX <mg_mesquita@hotmail.com>
2021-03-29 14:52:38 +0100

    Fetching and saving the code of each submission

--- index.js
+++ index.js
@@ -13,10 +13,28 @@ const contestSlug = process.env.CONTEST;
 const resultsPath = 'results';
 const contestPath = path.join(resultsPath, contestSlug);
 
+// map language to file extension
+const fileExtensions = {
+    cpp: 'cpp',
+    cpp14: 'cpp',
+    csharp: 'cs',
+    java: 'java',
+    java8: 'java',
+    javascript: 'js',
+    python: 'py'
+};
+
 
 async function getContestSubmissions() {
+    // get all contest submissions
     const response = await axios.get(urls.contestSubmissions(contestSlug));
-    return response.data.models;
+
+    // filter submissions that are code and whose file extension is known
+    return response.data.models.filter((submission) => {
+        return Object.keys(fileExtensions).includes(submission.language) &&
+            submission.in_contest_bounds &&
+            submission.kind === 'code'
+    });
 }
 
 function separateSubmissionsPerUser(submissions) {
@@ -44,20 +62,36 @@ function separateSubmissionsPerUserPerChallenge(submissions) {
     }, {});
 }
 
+function createFolder(path) {
+    if (!fs.existsSync(path))
+        fs.mkdirSync(path);
+}
+
 function createContestFolder() {
-    if (!fs.existsSync(resultsPath)) {
-        fs.mkdirSync(resultsPath);
-    }
-    if (!fs.existsSync(contestPath)) {
-        fs.mkdirSync(contestPath);
-    }
+    createFolder(resultsPath);
+    createFolder(contestPath);
 }
 
 function saveJSONFile(data, filename) {
     const filePath = path.join(contestPath, `${filename}.json`);
-    fs.writeFile(filePath, JSON.stringify(data,null,2), (err) => {
+    fs.writeFileSync(filePath, JSON.stringify(data,null,2), (err) => {
         if (err) console.log(err);
     });
+}
+
+async function fetchSubmissionCode(submissionId) {
+    // fetch submission info
+    const response = await axios.get(urls.submissionInfo(contestSlug, submissionId))
+    const submission = response.data.model;
+
+    // create user folder
+    const userPath = path.join(contestPath, submission.hacker_username);
+    createFolder(userPath);
+
+    // save submission code
+    const filename = `${submission.challenge_slug}-${submissionId}.${fileExtensions[submission.language]}`;
+    const filePath = path.join(userPath, filename);
+    fs.writeFileSync(filePath, submission.code);
 }
 
 
@@ -69,10 +103,13 @@ function saveJSONFile(data, filename) {
         const submissionsPerUserPerChallenge = separateSubmissionsPerUserPerChallenge(submissions);
 
         // save general info about submissions
-        createContestFolder();
+        createContestFolder(resultsPath);
         saveJSONFile(submissions, 'submissions');
         saveJSONFile(submissionsPerUser, 'submissionsPerUser');
         saveJSONFile(submissionsPerUserPerChallenge, 'submissionsPerUserPerChallenge');
+
+        // fetch and save the code of each submission
+        await Promise.all(submissions.map(submission => submission.id).map(fetchSubmissionCode));
     }
     catch (error) {
         console.error("An error occured: %d", error.statusCode);
@@ -81,7 +118,6 @@ function saveJSONFile(data, filename) {
 })();
 
 /* TODO
- - call submissionInfo API and save the code of each user's submission
  - add prints to inform user about what is happening
  - detect users with more than 1 submissions per week
  - detect users copying from each other
